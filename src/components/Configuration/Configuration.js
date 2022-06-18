4f39c6d31638ab1612e137a4f0afc5311df0b5ee
rakibul-cse-diu <rakibul2180@gmail.com>
2022-06-18 22:26:23 +0600

    setup layout

---
+++ src/components/Configuration/Configuration.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Configuration.css';
+
+const Configuration = () => {
+    return (
+        <div className='config-container'>
+            <div>
+                <h1>Configuration</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Configuration;
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Timer.css';
+
+const Timer = () => {
+    return (
+        <div className='timer-container'>
+            <div>
+                <h1>Timer</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Timer;
