f3becb5bac4b9e755f85bf2194f81dad954a5d03
rakibul-cse-diu <rakibul2180@gmail.com>
2022-06-18 23:29:27 +0600

    create timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './Timer.css';
 
 const Timer = () => {
     return (
         <div className='timer-container'>
-            <div>
-                <h1>Timer</h1>
+            <div className='progress-bar'>
+                <CircularProgressbar
+                    className=''
+                    value={40}
+                    text={"60:00"}
+                    styles={buildStyles({
+                        textSize: '16px',
+                        textColor: '#fff',
+                        pathColor: "red",
+                        // tailColor: "green",
+                    })} />
             </div>
         </div>
     );
